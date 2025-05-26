47547fd18669313384676303889833ca44cad45c
LeMinhTris <leminhtri260903@gmail.com>
2025-05-27 05:17:38 +0700

    fix data github

--- src/main/java/com/hkt/app/App.java
+++ src/main/java/com/hkt/app/App.java
@@ -2,15 +2,26 @@ package com.hkt.app;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class App extends Application {
     @Override
     public void start(Stage stage) throws Exception {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("/view/main.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("JavaFX Team App");
+        // Load giao diện từ FXMLs
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/view/dashboard.fxml"));
+        Parent root = fxmlLoader.load();
+
+        // Tạo Scene từ root
+        Scene scene = new Scene(root);
+
+        stage.setWidth(1220);
+        stage.setHeight(660);
+        stage.setResizable(false);
+
+        // Cài đặt title và scene
+        stage.setTitle("Convenient Store Management");
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/com/hkt/app/controller/DashboardController.java
+++ src/main/java/com/hkt/app/controller/DashboardController.java
@@ -3,30 +3,20 @@ package com.hkt.app.controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.net.URL;
-import java.util.ResourceBundle;
 
-public class DashboardController implements Initializable {
+public class DashboardController {
 
     @FXML
     private ImageView avatarImage1;
-
-    @FXML
-    private Button btnAdd;
-
-    @FXML
-    private Button btnProducts;
 
     @FXML
     private TableColumn<?, ?> colFullname;
@@ -41,56 +31,66 @@ public class DashboardController implements Initializable {
     private AnchorPane tvCustomer;
 
     @FXML
-    public void changToProduct(ActionEvent event) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/view/product.fxml"));
+    void changToAdd(ActionEvent event) throws IOException {
+        // Load giao diện từ FXMLs
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/view/addProduct.fxml"));
         Parent root = fxmlLoader.load();
 
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
 
+        // Tạo Scene từ root
         Scene scene = new Scene(root);
+
         stage.setWidth(1220);
         stage.setHeight(660);
         stage.setResizable(false);
 
-        stage.setTitle("Convenient Store Management");
-        stage.setScene(scene);
-        stage.show();
-    }
-
-    public void changToDashboard(ActionEvent event) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/view/dashboard.fxml"));
-        Parent root = fxmlLoader.load();
-
-        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-
-        Scene scene = new Scene(root);
-        stage.setWidth(1220);
-        stage.setHeight(660);
-        stage.setResizable(false);
-
+        // Cài đặt title và scene
         stage.setTitle("Convenient Store Management");
         stage.setScene(scene);
         stage.show();
     }
 
     @FXML
-    void changToAdd(ActionEvent event) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/view/addProduct.fxml"));
+    void changToDashboard(ActionEvent event) throws IOException {
+        // Load giao diện từ FXMLs
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/view/dashboard.fxml"));
         Parent root = fxmlLoader.load();
 
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
 
+        // Tạo Scene từ root
         Scene scene = new Scene(root);
+
         stage.setWidth(1220);
         stage.setHeight(660);
         stage.setResizable(false);
 
+        // Cài đặt title và scene
         stage.setTitle("Convenient Store Management");
         stage.setScene(scene);
         stage.show();
     }
 
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
+    @FXML
+    void changToProduct(ActionEvent event) throws IOException {
+        // Load giao diện từ FXMLs
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/view/product.fxml"));
+        Parent root = fxmlLoader.load();
+
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+        // Tạo Scene từ root
+        Scene scene = new Scene(root);
+
+        stage.setWidth(1220);
+        stage.setHeight(660);
+        stage.setResizable(false);
+
+        // Cài đặt title và scene
+        stage.setTitle("Convenient Store Management");
+        stage.setScene(scene);
+        stage.show();
     }
+
 }
